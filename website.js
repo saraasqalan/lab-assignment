ada853513b793c7d1f2c7c2eac6c4937e04011f4
saraasqalan <asqalansara@gmail.com>
2021-01-27 15:33:57 +0200

    js

--- website.js
+++ website.js
@@ -3,17 +3,17 @@ var reson = prompt("why you prefer this website ?");
 var CourseNo = prompt("how many course you want take ?");
 
 var CourseDetails ;
-if(CourseNo =1){
-    CourseDetails="No Discount";
-}
- if (CourseNo >1 && CourseNo <3){
+if(CourseNo >=1 && CourseNo <3){
     CourseDetails="congratulation you have 25% Discount";
 }
-if (CourseNo >3 && CourseNo <5){
+ if (CourseNo >=3 && CourseNo <5){
     CourseDetails="congratulation you have 50% Discount";
 }
+if (CourseNo >=5){
+    CourseDetails="congratulation you have 50% Discount and 1 more course for free !!";
+}
 else{
-CourseDetails="congratulation you have 50% Discount and 1 more course for free !!";
+CourseDetails="sorry you write wrong value :)";
 }
 
 document.write(CourseDetails);
