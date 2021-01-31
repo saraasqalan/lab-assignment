16fc5f6b8e9d69aee3f6b2465e8009537136a65a
saraasqalan <asqalansara@gmail.com>
2021-01-31 16:09:15 +0200

    edit

--- website.js
+++ website.js
@@ -1,10 +1,16 @@
-var website = prompt("which web site you Preferred start class with it ?");
+var website; 
+function getWebsite() {
+website = prompt("which web site you Preferred start class with it ?");
 while (website !=='wix' && website !=='w3schools' && website !=='udemy'){
     website = prompt("which web site you Preferred start class with it ?");
-}
+}}
+getWebsite();
 alert ("ok");
 
-var CourseNo = prompt("how many course you want take ?");
+var CourseNo ;
+function getCourseNo(){
+CourseNo = prompt("how many course you want take ?");}
+getCourseNo();
 alert("ok");
 
 function getName() {
@@ -19,10 +25,13 @@ function getName() {
 
 
 var number = '';
-for (var i = 0; i <= CourseNo; i++) {
-  number = number +course ;
 var course = '';
+for (var i = 0; i <= CourseNo; i++) {
+  
+  
+
 if (website === 'wix') {
+
   course = '<img src="image/wix.png"/ >' ;
 } 
 else if (website === 'udemy') {
@@ -30,22 +39,7 @@ else if (website === 'udemy') {
 }
  else if (website === 'w3schools') {
     course = '<img src="image/w3schools.png"/>';
-    }}
+    }
+    number = number +course ;}
   
-var CourseDetails ;
-if(CourseNo =1){
-    CourseDetails="no Discount";
-}
-else if (CourseNo >1 && CourseNo <3){
-    CourseDetails="congratulation you have 25% Discount";
-}
-else if (CourseNo >3 && CourseNo <5){
-    CourseDetails="congratulation you have 50% Discount";
-}
-else{
-CourseDetails="congratulation you have 50% Discount and 1 more course for free !!";}
-
-
-document.write(CourseDetails);
-document.write(course);
 document.write(number);
